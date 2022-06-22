d247a1030ef5c4b6232ef1cb8c2ff8aaa60a8542
Bianca Oliveira <102302275+biancaoliv@users.noreply.github.com>
2022-06-22 19:08:00 -0300

    Update main.js

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,5 +1,9 @@
 import './style.css'
 
+window.insert = function insert(num) {
+    document.getElementById('resultado').innerHTML = num;
+}
+
 function insert(num) {
     document.getElementById('resultado').innerHTML = num;
 }
